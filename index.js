db1b2de55d351e59ca49e704f8b7884cadf7e0e1
InfiniteHunter22 <56155403+InfiniteHunter22@users.noreply.github.com>
2019-10-04 20:07:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const discord = require ('discord.js');
-var client = new discord.Client();
-client.on("ready", () => {
-    readyEmbed = new discord.RichEmbed()
-    .setTitle ("**\n\nBot is now online**")
-    .setColor ("GREEN")
-    .setThumbnail ("https://cdn.discordapp.com/attachments/579599012383031298/599154058946674688/PandaDuckCop.png")
-    .setFooter("!help for assistance.")
-    .setTimestamp()
-    client.channels.get("579599012383031298").send(readyEmbed)
-    console.log("ready!");
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
 
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);
-
-const prefix = "!";
-client.on ("message", (message) => {
-
-    if (message.author.bot) return;
-
-    msg = message.content.toLowerCase();
-    
- if (msg.startsWith ("test") {
-     message.channel.send ("hey there, its working omg")
-     }
