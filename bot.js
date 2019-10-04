fba942444b4a6d66e1577424544fa1be90448276
InfiniteHunter22 <56155403+InfiniteHunter22@users.noreply.github.com>
2019-10-04 18:05:34 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,24 @@
+const discord = require ('discord.js');
+
+var client = new discord.Client();
+
+const token = "NTc5MjA5NTc1NjM1NDg0Njgy.XZZFHg.sq3kSGiFVAwNmFT_vRJQaJDUQNQ";
+
+client.on("ready", () => {
+    readyEmbed = new discord.RichEmbed()
+    .setTitle ("**\n\nBot is now online**")
+    .setColor ("GREEN")
+    .setThumbnail ("https://cdn.discordapp.com/attachments/579599012383031298/599154058946674688/PandaDuckCop.png")
+    .setFooter("!help for assistance.")
+    .setTimestamp()
+    client.channels.get("579599012383031298").send(readyEmbed)
+    console.log("ready!");
+
+    client.user.setActivity ("Infi's decisions", {type: "Listening"});
+
+    answered = true;
+    cAnswer = "";
+    userAnswer = "";
+});
+
+client.login(process.env.BOT_TOKEN);
